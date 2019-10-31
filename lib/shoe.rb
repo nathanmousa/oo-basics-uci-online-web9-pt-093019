ebd738668b5d56e0bc77416ab6e59b1593cdd958
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-31 01:06:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,10 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
   end
-  
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
   
   def cobble
     puts "Your shoe is as good as new!"
